4b32e43cc854ed3ce1c7d2e65a11e552dd7fa770
abi <abinayainwork@gmail.com>
2025-09-06 20:18:20 +0530

    App.jsx file is added

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import EventTopics from "../src/components/EventTopics";
+import "./App.css";
+import Crud from "./components/Crud";
+// import Chumma from './components/Chumma'
+
+const App = () => {
+  return (
+    <div>
+      {/* <EventTopics /> */}
+      {/* <Chumma/> */}
+      <Crud />
+    </div>
+  );
+};
+
+export default App;
